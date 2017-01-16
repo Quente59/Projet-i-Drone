e23faf8dca73a0476c46e2458241f413a5da5eef
Quente59 <quentin.lucas@depinfonancy.net>
2017-01-16 01:43:33 +0100

    Ajout publisher subscriber

--- ROStutorial/catkin_ws/src/beginner_tutorials/scripts/talker.py
+++ ROStutorial/catkin_ws/src/beginner_tutorials/scripts/talker.py
@@ -1,54 +1,55 @@
 #!/usr/bin/env python
-
-
+# license removed for brevity
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
+
 x = 0
 global x
 
-def callback(position):
 
-    global x
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', position)
+def talker():
 
-    x = position.x
-
-def motor():
     global x
     
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('motor', anonymous=True)
+    rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
 
     rospy.Subscriber('/turtle1/pose', Pose, callback)
     
-
     while not rospy.is_shutdown():
-	message = Twist()
-
         
 
-	if x < 10:
-
-            message.linear.x = 2.0
-	    pub.publish(message)
-            rate.sleep()
-
-	else:
-            
-	    message.linear.x = 0.0
-	    message.angular.z = 5.0
+	if x < 10.0 :
+            message = Twist()
+	    message.linear.x = 2.0
+	    message.angular.z = 0.0
+            #rospy.loginfo(message)
             pub.publish(message)
             rate.sleep()
 
-	
-        
+        else :
+            message = Twist()
+	    message.linear.x = 0.0
+	    message.angular.z = 5.0
+            #rospy.loginfo(message)
+            pub.publish(message)
+            rate.sleep()
+
+
+def callback(position):
+    global x
     
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', position)
+    x = position.x
+    
+
+
 
 if __name__ == '__main__':
     try:
-        motor()
+        talker()
     except rospy.ROSInterruptException:
         pass
