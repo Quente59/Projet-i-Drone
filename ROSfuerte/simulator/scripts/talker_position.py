96eae3e8aa44f18f089fb0a5579c0ba61e6c92c5
leecaro <caroline.lee@depinfonancy.net>
2017-05-15 21:23:29 +0200

    updated talker_position.py
    le message retourné dans le topic /ardrone/position est désormais une position
    on peut appeler : position.x, position.y, position.z, orientation.x, etc pour avoir les coordonnées du quadrotor

--- ROSfuerte/simulator/scripts/talker_position.py
+++ ROSfuerte/simulator/scripts/talker_position.py
@@ -14,37 +14,42 @@ from gazebo_msgs.msg import *
 global model
 model = ModelState()
 
+global position
+position = Pose()
+
 #ModelStates() correspond a 3 listes : name[], pose[], twist[]
 #ModelState() contient name, pose, twist etc. pour un element
 
 def talker_position():
 
     global model
-
+    global position
 
     rospy.init_node('talker_position', anonymous=True)
-    pub = rospy.Publisher('/ardrone/position', ModelState)
+    pub = rospy.Publisher('/ardrone/position', Pose)
     
     rate = rospy.Rate(10) # 10hz
 
     rospy.Subscriber('/gazebo/model_states', ModelStates , callback)
 
-
     while not rospy.is_shutdown():
 
-        pub.publish(model)
+        pub.publish(position)
         rate.sleep()
 
 
 def callback(model_states):
 
     global model
+    global position
+
     for i in range(len(model_states.name)):
 	if (model_states.name[i] == 'quadrotor'):
     	    model.model_name = model_states.name[i]
 	    model.pose = model_states.pose[i]
 	    model.twist = model_states.twist[i]
 
+    position = model.pose
 
 
 if __name__ == '__main__':
